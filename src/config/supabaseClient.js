c1165d7ef0c4255e5cd7a6427c52fd0d9cba9217
Fikkung <study0001fix@gmail.com>
2025-12-16 17:03:13 +0700

    fix: update main entry point and scripts in package.json; refactor supabaseClient.js imports

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config(); // ไม่ต้องใส่ path
+
+import express from "express";
+// import routes / config ต่อจากนี้
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
 
-
-dotenv.config({ path: '../.env' });
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
